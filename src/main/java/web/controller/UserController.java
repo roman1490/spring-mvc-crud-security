d58ba50e20494e791495606fcc8c2f796f2272f9
Roman Perevalov <roman1490@yandex.ru>
2021-01-31 21:06:08 +0700

    hello page removed

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -5,14 +5,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import web.model.Role;
 import web.model.User;
 import web.service.RoleService;
 import web.service.UserService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -30,16 +28,6 @@ public class UserController {
     @Autowired
     public void setRoleService(RoleService roleService) {
         this.roleService = roleService;
-    }
-
-    @RequestMapping(value = "hello", method = RequestMethod.GET)
-    public String printWelcome(ModelMap model) {
-        List<String> messages = new ArrayList<>();
-        messages.add("Hello!");
-        messages.add("I'm Spring MVC-SECURITY application");
-        messages.add("5.2.0 version by sep'19 ");
-        model.addAttribute("messages", messages);
-        return "hello";
     }
 
     @RequestMapping(value = "login", method = RequestMethod.GET)
--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -1,11 +1,9 @@
 package web.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import web.model.User;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
@@ -55,81 +53,3 @@ public class UserDaoImpl implements UserDao {
         return entityManager.createQuery("from User").getResultList();
     }
 }
-
-//package web.dao;
-//
-//        import org.springframework.beans.factory.annotation.Autowired;
-//        import org.springframework.stereotype.Repository;
-//        import web.model.User;
-//
-//        import javax.persistence.EntityManager;
-//        import javax.persistence.EntityManagerFactory;
-//        import java.util.List;
-//
-//@Repository
-//public class UserDaoImpl implements UserDao {
-//
-//    private final EntityManagerFactory entityManagerFactory;
-//
-//    @Override
-//    public User getUserByEmail(String email) {
-//        EntityManager em = entityManagerFactory.createEntityManager();
-//        return (User) em.createQuery("from User where email = :email")
-//                .setParameter("email", email)
-//                .getSingleResult();
-//    }
-//
-//    @Autowired
-//    public UserDaoImpl(EntityManagerFactory entityManagerFactory) {
-//        this.entityManagerFactory = entityManagerFactory;
-//    }
-//
-//    @Override
-//    public User getUserById(int id) {
-//        EntityManager entityManager = entityManagerFactory.createEntityManager();
-//        return entityManager.find(User.class, id);
-//    }
-//
-//    @Override
-//    public void addUser(User user) {
-//        EntityManager entityManager = entityManagerFactory.createEntityManager();
-//        entityManager.getTransaction().begin();
-//        entityManager.persist(user);
-//        entityManager.getTransaction().commit();
-//    }
-//
-//    @Override
-//    public void updateUser(int id, User newUser) {
-//        EntityManager entityManager = entityManagerFactory.createEntityManager();
-//        entityManager.getTransaction().begin();
-//
-//        User user = entityManager.find(User.class, id);
-//        user.setFirstName(newUser.getFirstName());
-//        user.setLastName(newUser.getLastName());
-//        user.setAge(newUser.getAge());
-//        user.setEmail(newUser.getEmail());
-//        user.setPassword(newUser.getPassword());
-//        user.setRoles(newUser.getRoles());
-//
-//        entityManager.getTransaction().commit();
-//    }
-//
-//    @Override
-//    public void deleteUser(int id) {
-//        EntityManager entityManager = entityManagerFactory.createEntityManager();
-//        entityManager.getTransaction().begin();
-//
-//        User user = entityManager.find(User.class, id);
-//        entityManager.remove(user);
-//
-//        entityManager.getTransaction().commit();
-//    }
-//
-//    @Override
-//    @SuppressWarnings("unchecked")
-//    public List<User> getAllUsers() {
-//        EntityManager entityManager = entityManagerFactory.createEntityManager();
-//        return entityManager.createQuery("from User").getResultList();
-//    }
-//}
-
